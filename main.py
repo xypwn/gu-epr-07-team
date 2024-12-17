cec04d01b30bc5d1daf7c3c638add20727c22295
xypwn <54681180+xypwn@users.noreply.github.com>
2024-12-17 13:38:06 +0100

    init

---
+++ main.py
@@ -0,0 +1,315 @@
+__author__ = "8030456, Schuppan"
+
+from dataclasses import dataclass, field
+from datetime import datetime
+
+import shell
+import util
+
+
+@dataclass
+class FoodItem:
+    name: str
+    type: str
+    categories: set[str]
+    price: int  # In cents.
+
+
+class FoodItems:
+    def __init__(self, filename: str):
+        self.__items = []
+        with open(filename, "r") as file:
+            for i, line in enumerate(file):
+                if i == 0 or line == "":
+                    continue
+                cols = line.removesuffix("\n").split(";")
+                if len(cols) != 4:
+                    raise Exception(
+                        f"line {i}: expected semicolon- separated \
+    CSV with 4 columns (name, type, category, price)"
+                    )
+                categories = set(s.strip() for s in cols[2].split(","))
+                price = 0.0
+                try:
+                    price = float(cols[3].replace(",", ".")) * 100
+                except ValueError:
+                    raise Exception(
+                        f"line {i}: expected price \
+    (3rd column) to be a floating-point number"
+                    )
+                if price % 1 != 0:
+                    raise Exception(
+                        f"line {i}: expected price (3rd column) to be \
+    at most accurate to the 0.01 decimal place (cents)"
+                    )
+                price = int(price)
+                self.__items.append(
+                    FoodItem(cols[0], cols[1], categories, price)
+                )
+
+    def __iter__(self):
+        return iter(self.__items)
+
+    def __getitem__(self, i):
+        return self.__items[i]
+
+    def __len__(self):
+        return len(self.__items)
+
+
+@dataclass
+class SpecialRequest:
+    request: str
+    charge: int  # In cents.
+
+
+@dataclass
+class Order:
+    time: datetime
+    food_item: FoodItem
+    special_requests: list[SpecialRequest]
+
+    def amount(self) -> int:
+        """Returns the total amount in cents."""
+        return self.food_item.price + sum(
+            r.charge for r in self.special_requests
+        )
+
+
+@dataclass
+class Rescindment:
+    time: datetime
+    item_id: int
+    price: int
+
+    def amount(self) -> int:
+        """Returns the total amount in cents."""
+        return -self.price
+
+
+@dataclass
+class Table:
+    id: str
+    orders: list[Order | Rescindment] = field(default_factory=list)
+
+    def amount(self) -> int:
+        """Returns the total amount in cents."""
+        return sum(o.amount() for o in self.orders)
+
+
+class App(shell.Shell):
+    def __init__(self, food_items_filename):
+        super().__init__()
+        self.food_items = FoodItems(food_items_filename)
+        self.tables: dict[str, Table] = {}
+        self.curr_table = None
+
+
+def run():
+    app = App("food.csv")
+
+    def cmd_table(self, params: list[object]) -> None:
+        table = params[0]
+        if (
+            self.curr_table is not None
+            and self.tables[self.curr_table] is not None
+            and table == self.curr_table
+        ):
+            print(f"Table {table} already selected.")
+            return
+        new = ""
+        if table not in self.tables:
+            new = "new "
+            self.tables[table] = Table(table)
+        print(f'Switched to {new}table "{table}".')
+        self.curr_table = table
+        self.set_prompt_prefix([f"table={table}"])
+
+    def cmd_tables(self, params: list[object]) -> None:
+        if len(self.tables) == 0:
+            print("No tables.")
+        else:
+            print("Tables:")
+            rows = []
+            for name in sorted(self.tables.keys()):
+                table = self.tables[name]
+                orders = len(table.orders) if table.orders else "no"
+                plural = "" if len(table.orders) == 1 else "s"
+                rows.append(
+                    [
+                        f" * {name}",
+                        f"{orders} order{plural}",
+                        f"{table.amount()/100}€",
+                    ]
+                )
+            print(util.column_align(rows, sep="  "))
+
+    def cmd_list(self, params: list[object]) -> None:
+        print("Food items:")
+        rows = [["No.", "Name", "Type", "Tags", "Price"]]
+        for i, item in enumerate(self.food_items):
+            search_str = f"{item.name} {item.type} {item.categories}"
+            if params[0] is not None:
+                if params[0].lower() not in search_str.lower():
+                    continue
+            rows.append(
+                [
+                    f" {i+1}.",
+                    item.name,
+                    item.type,
+                    ",".join(item.categories),
+                    f"{item.price/100}€",
+                ]
+            )
+        print(util.column_align(rows, sep="  "))
+
+    def cmd_order(self, params: list[object]) -> None:
+        if self.curr_table is None or self.tables[self.curr_table] is None:
+            print("Must select a table before placing an order.")
+            print('Use the "table" command to create/select a table.')
+            return
+        curr_table = self.tables[self.curr_table]
+
+        item = self.food_items[params[0] - 1]
+        special_requests = []
+        while True:
+            print(
+                f"Ordering {item.name} for \
+table {curr_table.id}."
+            )
+            if len(special_requests) > 0:
+                print("Special requests:")
+                for req in special_requests:
+                    print(f" * {req.request} ({req.charge/100}€)")
+            print("Options:")
+            print("  y: Confirm")
+            print("  n: Cancel")
+            print("  s: Add special request")
+            sel = input("Selection [Yns]: ").lower()
+            if sel == "y" or sel == "":
+                curr_table.orders.append(
+                    Order(
+                        datetime.now(),
+                        item,
+                        special_requests,
+                    )
+                )
+                print("Order placed.")
+                break
+            elif sel == "n":
+                print("Order cancelled.")
+                break
+            elif sel == "s":
+                req = input("Special request: ")
+                charge = (
+                    input("Charge for 1€ for special request? [yN]: ").lower()
+                    == "y"
+                )
+                special_requests.append(
+                    SpecialRequest(req, 100 if charge else 0)
+                )
+            else:
+                print("Invalid option.")
+            print()
+
+    def cmd_orders(self, params: list[object]) -> None:
+        if self.curr_table is None or self.tables[self.curr_table] is None:
+            print("No table selected.")
+            return
+        curr_table = self.tables[self.curr_table]
+
+        if len(curr_table.orders) == 0:
+            print(f"No orders for table {curr_table.id}.")
+            return
+
+        print(f"Orders for table {curr_table.id}:")
+        for i, order in enumerate(curr_table.orders):
+            if isinstance(order, Order):
+                print(f" {i+1}. {order.food_item.name} \
++{order.food_item.price/100}€")
+                for req in order.special_requests:
+                    print(f"  + {req.request} \
+({req.charge/100}€)")
+            elif isinstance(order, Rescindment):
+                print(f" {i+1}. Rescind order no. \
+{order.item_id+1} -{order.price/100}€")
+            else:
+                raise ValueError
+        print(f"Total: {curr_table.amount()/100}€")
+
+    def cmd_rescind(self, params: list[object]) -> None:
+        if self.curr_table is None or self.tables[self.curr_table] is None:
+            print("No table selected.")
+            return
+        curr_table = self.tables[self.curr_table]
+
+        order_id = params[0] - 1
+        if order_id >= len(curr_table.orders):
+            print(
+                'Invalid order ID. Use "orders" \
+to list orders.'
+            )
+            return
+        order = curr_table.orders[order_id]
+        if not isinstance(order, Order):
+            print("Can only rescind orders.")
+            return
+        curr_table.orders.append(
+            Rescindment(datetime.now(), order_id, order.amount())
+        )
+        print(f"Rescinded order {order_id+1} \
+({order.food_item.name}) for {order.amount()/100}€.")
+
+    app.add_command(
+        shell.Command(
+            "table",
+            "create and/or switch active table",
+            [shell.StringParam("table_name")],
+            cmd_table,
+        )
+    )
+    app.add_command(shell.Command("tables", "list tables", [], cmd_tables))
+    app.add_command(
+        shell.Command(
+            "list",
+            "list available food items and their IDs",
+            [shell.StringParam("filter", optional=True)],
+            cmd_list,
+        )
+    )
+    app.add_command(
+        shell.Command(
+            "order",
+            "place an order for the current table",
+            [
+                shell.IntParam(
+                    "item_id",
+                    min=1,
+                    max=len(app.food_items),
+                )
+            ],
+            cmd_order,
+        )
+    )
+    app.add_command(
+        shell.Command(
+            "orders",
+            "list current table's orders",
+            [],
+            cmd_orders,
+        )
+    )
+    app.add_command(
+        shell.Command(
+            "rescind",
+            "rescind one of the current table's orders",
+            [shell.IntParam("order_id", min=1)],
+            cmd_rescind,
+        )
+    )
+
+    app.run("Welcome to the RESTAURANT SHELL 9000!")
+
+
+if __name__ == "__main__":
+    run()
---
+++ shell.py
@@ -0,0 +1,227 @@
+from dataclasses import dataclass, field
+from enum import Enum
+from typing import Callable
+from abc import ABC, abstractmethod
+
+import util
+
+
+class Param(ABC):
+    @abstractmethod
+    def optional() -> bool:
+        pass
+
+    @abstractmethod
+    def name(self) -> str:
+        pass
+
+    @abstractmethod
+    def constraints(self) -> str:
+        pass
+
+    @abstractmethod
+    def parse(self, val: str) -> object:
+        pass
+
+
+class StringParam(Param):
+    def __init__(self, name: str, optional: bool = False) -> None:
+        self.__name = name
+        self.__optional = optional
+
+    def optional(self) -> str:
+        return self.__optional
+
+    def name(self) -> str:
+        return self.__name
+
+    def constraints(self):
+        return "string"
+
+    def parse(self, val: str) -> str:
+        return val
+
+
+class IntParam(Param):
+    def __init__(
+        self,
+        name: str,
+        optional: bool = False,
+        min: int | None = None,
+        max: int | None = None,
+    ) -> None:
+        self.__name = name
+        self.__optional = optional
+        self.__min = min
+        self.__max = max
+
+    def optional(self) -> str:
+        return self.__optional
+
+    def name(self) -> str:
+        return self.__name
+
+    def constraints(self) -> str:
+        res = "int"
+        if self.__min is not None:
+            res += f" from {self.__min}"
+        if self.__max is not None:
+            res += f" to {self.__max}"
+        return res
+
+    def parse(self, val: str) -> float:
+        i = 0
+        try:
+            i = int(val)
+        except:
+            raise ValueError("expected integer")
+        if self.__min is not None:
+            if i < self.__min:
+                raise ValueError(
+                    f"expected number to be at least {self.__min}"
+                )
+        if self.__max is not None:
+            if i > self.__max:
+                raise ValueError(f"expected number to be at most {self.__max}")
+        return i
+
+
+@dataclass
+class Command:
+    """Command is a command that can be executed in the shell.
+    The signature for the run callback is inteded as follows:
+    def my_cmd(self, params: list[object]) -> None:"""
+
+    name: str
+    description: str
+    params: list[Param]
+    run: Callable[[object, list[object]], None]
+
+
+class Shell:
+    """Shell is a generalized interactive shell which can
+    execute commands via callbacks. Commands can be registered
+    using add_command."""
+
+    def __init__(self) -> None:
+        self.__prompt_elems: list[str] = []
+        self.__commands: dict[str, Command] = {}
+
+    def add_command(self, cmd: Command) -> None:
+        """Adds a new command to the shell. Raises an
+        an exception if the command was invalid."""
+        seen_optional_param = False
+        for param in cmd.params:
+            if seen_optional_param and not param.optional():
+                raise ValueError("only the last parameters may be optional")
+            if param.optional():
+                seen_optional_param = True
+
+        self.__commands[cmd.name] = cmd
+
+    def help(self) -> str:
+        """Print a nicely formatted help page listing
+        commands and their parameters and descriptions."""
+
+        rows = [
+            ["exit", "exit the shell"],
+            ["help", "show this page"],
+        ]
+
+        for cmd in self.__commands.values():
+            words = [cmd.name]
+            for p in cmd.params:
+                word = f"{p.name()}: {p.constraints()}"
+                if p.optional():
+                    word = f"[{word}]"
+                else:
+                    word = f"<{word}>"
+                words.append(word)
+            rows.append([" ".join(words), cmd.description])
+
+        for row in rows:
+            row[0] = "  " + row[0]
+
+        prolog = [
+            'Command format: "command <required_parameter> \
+[optional_parameter]"',
+            "Commands:",
+        ]
+
+        return "\n".join(prolog) + "\n" + util.column_align(rows, sep=" - ")
+
+    def set_prompt_prefix(self, elems: list[str]) -> None:
+        """Sets the prefix words which will be printed
+        before each prompt, separated by spaces."""
+        self.__prompt_elems = elems
+
+    def run(self, init_prompt: str) -> None:
+        """Runs the main shell loop."""
+        print(init_prompt)
+        print('Type "help" for a list of commands.')
+        while True:
+            try:
+                prompt = " ".join(self.__prompt_elems + [">> "])
+                args = input(prompt).split()
+                if len(args) == 0:
+                    continue
+                if args[0] == "help":
+                    print(self.help())
+                elif args[0] == "exit" or args[0] == "quit":
+                    print("Exiting.")
+                    break
+                elif args[0] in self.__commands:
+                    cmd = self.__commands[args[0]]
+                    num_params = len(args) - 1
+                    min_params = sum(
+                        0 if p.optional else 1 for p in cmd.params
+                    )
+                    max_params = len(cmd.params)
+                    if num_params < min_params:
+                        plural = "" if min_params == 1 else "s"
+                        print(f"{args[0]} expects at least \
+{min_params} parameter{plural}.")
+                        continue
+                    if num_params > max_params:
+                        if max_params == 0:
+                            print(
+                                f"{args[0]} expects no \
+parameters."
+                            )
+                        else:
+                            print(
+                                f"{args[0]} expects at most \
+{max_params} parameters."
+                            )
+                        continue
+                    params_not_ok = False
+                    params = []
+                    for i, param in enumerate(cmd.params):
+                        if i + 1 >= len(args):
+                            # Optional param wasn't specified.
+                            params.append(None)
+                            continue
+                        try:
+                            params.append(param.parse(args[i + 1]))
+                        except Exception as e:
+                            print(
+                                f"Error: {cmd.name}: \
+{param.name}: {e}."
+                            )
+                            params_not_ok = True
+                            break
+                    if params_not_ok:
+                        continue
+                    cmd.run(self, params)
+                else:
+                    print(f'Unknwon command "{args[0]}"!')
+                    print('Type "help" for a list of commands.')
+                print()
+            except KeyboardInterrupt:
+                print()
+                print('Type "exit" or "quit", or ctrl+d to quit.')
+                continue
+            except EOFError:
+                print()
+                print("Exiting.")
+                break
